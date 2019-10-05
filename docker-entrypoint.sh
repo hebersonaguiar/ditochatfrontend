07eec28a59a6dc0ac9a616cb8888d4d53bc262e5
Heberson Aguiar <hebersonaguiar@gmail.com>
2019-10-05 10:29:09 -0300

    [MODIFY] Inserindo npm start

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+cd /app &
 /usr/bin/npm install react@16.8.1 react-chat-elements@10.2.0 react-dom@16.8.1 react-router-dom@4.3.1 typescript react-scripts@2.1.5 & 
 /usr/bin/npm audit fix &
 /usr/bin/npm audit fix --force &
 
+usr/bin/npm start 
