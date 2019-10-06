3c9b543109d6263a557c14de534c72a25f13642e
Heberson Aguiar <hebersonaguiar@gmail.com>
2019-10-06 15:59:06 -0300

    [MODIFY] Corrigindo entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -19,11 +19,11 @@ if [ ! -z "$REACT_APP_BACKEND_WS" ] || [ ! -z "$REACT_APP_BACKEND_URL" ] ; then
       export -p
       source ~/.bash_profile 
 
-      cd /app
-      /usr/bin/npm install react@16.8.1 react-chat-elements@10.2.0 react-dom@16.8.1 react-router-dom@4.3.1 typescript react-scripts@2.1.5 
-      /usr/bin/npm audit fix
-      /usr/bin/npm audit fix --force
-      /usr/bin/npm start 
+      #cd /app
+      #/usr/bin/npm install react@16.8.1 react-chat-elements@10.2.0 react-dom@16.8.1 react-router-dom@4.3.1 typescript react-scripts@2.1.5 
+      #/usr/bin/npm audit fix
+      #/usr/bin/npm audit fix --force
+      #/usr/bin/npm start 
 
 else
 	echo "Please enter the required variables!"
