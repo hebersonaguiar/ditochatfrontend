fec653579372ea3c2f734ab342a95c9e68963341
Heberson Aguiar <hebersonaguiar@gmail.com>
2019-10-06 17:27:19 -0300

    [MODIFY] Alterando Dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,27 +3,28 @@
 set -e
 
 help() {
-    echo "Usage: docker run -dti -e REACT_APP_BACKEND_WS=<value1> -e REACT_APP_BACKEND_URL=<value2> image:tag" >&2
+    echo "Usage: docker run -dti -e REACT_APP_BACKEND_WS_URL=<value1> image:tag" >&2
     echo
-    echo "   REACT_APP_BACKEND_WS        Url for webservice chat ex: ws://0.0.0.0:8080"
-    echo "   REACT_APP_BACKEND_URL       Url for access backend ex: http://0.0.0.0:8080"
-
+    echo "   REACT_APP_BACKEND_WS_URL        Url for webservice chat ex: backend.example.com"
     echo
     exit 1
 }
 
-if [ ! -z "$REACT_APP_BACKEND_WS" ] || [ ! -z "$REACT_APP_BACKEND_URL" ] ; then
+if [ ! -z "$REACT_APP_BACKEND_WS_URL" ] ; then
 
-      echo "export REACT_APP_BACKEND_WS='$REACT_APP_BACKEND_WS'" >> ~/.bash_profile
-      echo "export REACT_APP_BACKEND_URL='$REACT_APP_BACKEND_URL'" >> ~/.bash_profile
+      REACT_APP_BACKEND_WS=$(dig +short $REACT_APP_BACKEND_WS_URL)
+      REACT_APP_BACKEND_URL=$(dig +short $REACT_APP_BACKEND_WS_URL)
+
+      echo "export REACT_APP_BACKEND_WS='ws://$REACT_APP_BACKEND_WS:8080'" >> ~/.bash_profile
+      echo "export REACT_APP_BACKEND_URL='https://$REACT_APP_BACKEND_URL:8080'" >> ~/.bash_profile
       export -p
       source ~/.bash_profile 
 
-      cd /app
+      #cd /app
       #/usr/bin/npm install react@16.8.1 react-chat-elements@10.2.0 react-dom@16.8.1 react-router-dom@4.3.1 typescript react-scripts@2.1.5 
       #/usr/bin/npm audit fix
       #/usr/bin/npm audit fix --force
-      /usr/bin/npm start 
+      #/usr/bin/npm start 
 
 else
 	echo "Please enter the required variables!"
