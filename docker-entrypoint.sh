0fced92f928114c8d8e2dbd3189163b9830c9acc
Heberson Aguiar <hebersonaguiar@gmail.com>
2019-10-06 18:52:27 -0300

    .

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,17 +12,21 @@ help() {
 
 if [ ! -z "$REACT_APP_BACKEND_WS_URL" ] ; then
 
-      dig +short $REACT_APP_BACKEND_WS_URL > /app/ipbackend
+      dig +short $REACT_APP_BACKEND_WS_URL > /app/IPBACKEND
+      
+      IPADDRESS=$(cat /app/IPBACKEND)
+      
+      sed -i "s/IPADDRESS/$IPADDRESS/g" /app/src/Chat.js
       
       #echo "export REACT_APP_BACKEND_WS='ws://$(cat /app/ipbackend):8080'" >> /etc/profile.d/env.sh
       #echo "export REACT_APP_BACKEND_URL='http://$(cat /app/ipbackend):8080'" >> /etc/profile.d/env.sh
       #source /etc/profile.d/env.sh
       
-      #cd /app
+      Cd /app
       #/usr/bin/npm install react@16.8.1 react-chat-elements@10.2.0 react-dom@16.8.1 react-router-dom@4.3.1 typescript react-scripts@2.1.5 
       #/usr/bin/npm audit fix
       #/usr/bin/npm audit fix --force
-      #/usr/bin/npm start 
+      /usr/bin/npm start 
 
 else
 	echo "Please enter the required variables!"
