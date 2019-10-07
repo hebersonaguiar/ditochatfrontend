b7325f5515f21fc4d4e7e859965f55b569701e46
Heberson Aguiar <hebersonaguiar@gmail.com>
2019-10-06 21:54:22 -0300

    .

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -23,9 +23,9 @@ if [ ! -z "$REACT_APP_BACKEND_WS_URL" ] ; then
       #source /etc/profile.d/env.sh
       
       cd /app
-      #/usr/bin/npm install react@16.8.1 react-chat-elements@10.2.0 react-dom@16.8.1 react-router-dom@4.3.1 typescript react-scripts@2.1.5 
-      #/usr/bin/npm audit fix
-      #/usr/bin/npm audit fix --force
+      /usr/bin/npm install react@16.8.1 react-chat-elements@10.2.0 react-dom@16.8.1 react-router-dom@4.3.1 typescript react-scripts@2.1.5 
+      /usr/bin/npm audit fix
+      /usr/bin/npm audit fix --force
       /usr/bin/npm start 
 
 else
